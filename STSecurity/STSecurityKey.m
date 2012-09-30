772047192407b4b7dc64849c0d213d7e3b88a616
Scott Talbot <s@chikachow.org>
2012-09-30 20:23:15 +1000

    License under the MPL 2.0

--- STSecurity/STSecurityKey.m
+++ STSecurity/STSecurityKey.m
@@ -2,6 +2,10 @@
 //  STSecurityKey.m
 //  STSecurity
 //
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
 //  Copyright (c) 2012 Scott Talbot. All rights reserved.
 //
 
