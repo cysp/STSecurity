31a9f9f3d1e8ad8ee30876503645d932b9b469a1
Scott Talbot <s@chikachow.org>
2012-10-01 12:51:56 +1000

    Add tests for STSecurityKey instantiation

---
+++ STSecurityTests/STSecurityKeyTests.h
@@ -0,0 +1,13 @@
+//
+//  STSecurityKeyTests.h
+//  STSecurity
+//
+//  Copyright (c) 2012 Scott Talbot. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface STSecurityKeyTests : SenTestCase
+
+@end
