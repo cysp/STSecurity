f35f561373a6d33b36b68e16c0fb61f8dd6d8382
Scott Talbot <s@chikachow.org>
2014-06-23 21:20:25 +1000

    Add Swift tests

---
+++ STSecurityTests/STSecurityTests-Bridging-Header.h
@@ -0,0 +1,6 @@
+//  Copyright (c) 2014 Scott Talbot. All rights reserved.
+
+#import <STSecurity/STSecurityRandomization.h>
+#import <STSecurity/STSecurityRSAKey.h>
+#import <STSecurity/STSecurityKeychainAccess.h>
+#import <STSecurity/STSecurityRSAEncryption.h>
